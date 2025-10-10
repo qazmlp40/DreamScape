bee83e76bcf7c28c3d061adeec4fc86a45e4507e
jisu <wltn1024000@gmail.com>
2025-10-10 22:33:33 +0900

    1차 커밋 완료

---
+++ src/main/java/signup/dreamscape/DreamScapeApplication.java
@@ -0,0 +1,13 @@
+package signup.dreamscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamScapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamScapeApplication.class, args);
+    }
+
+}
