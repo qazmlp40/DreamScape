2ec8caf3357874be936ce89861d6129b90154d8e
Jang <jhjang9326@naver.com>
2026-05-19 23:23:58 +0900

    코드 중복 오류 수정

--- src/main/java/signup/dreamscape/Controller/DreamController.java
+++ src/main/java/signup/dreamscape/Controller/DreamController.java
@@ -81,13 +81,4 @@ public class DreamController {
         dreamService.deleteDream(dreamId);
         return ResponseEntity.noContent().build();
     }
-
-    // (테스트용) dreamId로 더미 비디오 생성
-    @PostMapping("/media-test/{dreamId}")
-    public ResponseEntity<MediaResponseDTO> generateVideoFromDreamController(
-            @PathVariable Long dreamId
-    ) {
-        MediaResponseDTO response = mediaService.generateVideo(dreamId);
-        return ResponseEntity.ok(response);
-    }
 }
